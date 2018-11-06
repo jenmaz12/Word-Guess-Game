69cc85f711dd27572dbce275b175e23d52ce000e
jenmaz12 <jenmaz12@gmail.com>
2018-11-06 18:18:48 -0500

    more updates

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,20 +2,21 @@ var wordbank = ["chocolate","caramel","gumdrop","lollipop","licorice","taffy","t
 var lifepoints = 12;
 var wrongGuess = [];
 var computerWord;
+var computerWord = wordbank[Math.floor(Math.random()*wordbank.length)];
+document.getElementById("currentWord").innerHTML = "__ ".repeat(computerWord.length);
+
 
 document.onkeyup = function (event) {
     var userGuess = event.key.toLowerCase();
-    var computerWord = wordbank[Math.floor(Math.random()*wordbank.length)];
-    document.getElementById("currentWord").innerHTML = "__ ".repeat(computerWord.length);
     var foundinword = computerWord.search(userGuess);
     
 }
 var inWord = false;
 
-for (var i=0, i<computerWord.length, i++) {
+for (var i=0, i < computerWord.length, i++) {
     if (userGuess===computerWord.charAt (i)) {
         inWord = true;
-
+        document.getElementById("currentWord").innerHTML = userGuess
     }
     else {
 
