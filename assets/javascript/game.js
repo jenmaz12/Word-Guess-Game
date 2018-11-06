dde83e498b96ff3941dec7fc49886097dd7613a9
jenmaz12 <jenmaz12@gmail.com>
2018-11-05 19:58:33 -0500

    document.onkeyup and start for loop

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,5 +1,17 @@
 var wordbank = ["chocolate","caramel","gumdrop","lollipop","licorice","taffy","toffee","skittles","smarties","twix","snickers","raisinets"];
 var lifepoints = 12;
-var userGuess;
 var wrongGuess = [];
+var computerWord;
 
+document.onkeyup = function (event) {
+    var userGuess = event.key.toLowerCase();
+    var computerWord = wordbank[Math.floor(Math.random()*wordbank.length)];
+    var foundinword = computerWord.search(userGuess);
+}
+var inWord = false;
+for (var i=0, i<computerWord.length, i++) {
+    if (userGuess===computerWord.charAt (i)) {
+        inWord = true;
+        
+    }
+}
