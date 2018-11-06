e1e8528f27cfb53bdda216c6b209119563809a35
jenmaz12 <jenmaz12@gmail.com>
2018-11-05 19:20:22 -0500

    defining variables

---
+++ assets/javascript/game.js
@@ -0,0 +1,5 @@
+var wordbank = ["chocolate","caramel","gumdrop","lollipop","licorice","taffy","toffee","skittles","smarties","twix","snickers","raisinets"];
+var lifepoints = 12;
+var userGuess;
+var wrongGuess = [];
+
