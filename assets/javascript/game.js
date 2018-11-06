6af28ed52c92d4820ad18488a300064dd6c15c14
jenmaz12 <jenmaz12@gmail.com>
2018-11-05 20:41:45 -0500

    spaces

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -6,12 +6,18 @@ var computerWord;
 document.onkeyup = function (event) {
     var userGuess = event.key.toLowerCase();
     var computerWord = wordbank[Math.floor(Math.random()*wordbank.length)];
+    document.getElementById("currentWord").innerHTML = "__ ".repeat(computerWord.length);
     var foundinword = computerWord.search(userGuess);
+    
 }
 var inWord = false;
+
 for (var i=0, i<computerWord.length, i++) {
     if (userGuess===computerWord.charAt (i)) {
         inWord = true;
-        
+
+    }
+    else {
+
     }
 }
