9b80b7ed9f1cbd62583cf4d48b50a3f22299c3a0
jenmaz12 <jenmaz12@gmail.com>
2018-11-06 20:51:25 -0500

    spaces working

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,21 +1,25 @@
-var wordbank = ["chocolate","caramel","gumdrop","lollipop","licorice","taffy","toffee","skittles","smarties","twix","snickers","raisinets"];
-var lifepoints = 12;
-var wrongGuess = [];
-var computerWord = wordbank[Math.floor(Math.random()*wordbank.length)];
+// load window first
 window.onload = function() {
-    document.getElementById("currentWord").innerHTML = "__ ".repeat(computerWord.length);
-}
+// define word bank array
+var wordbank = ["chocolate","caramel","gumdrop","lollipop","licorice","taffy","toffee","skittles","smarties","twix","snickers","raisinets"];
+// define user life points at start
+var lifepoints = 12;
+// define array for wrong guesses to be pushed to
+var wrongGuess = [];
+// computer randomly selects word from word bank for current word
+var computerWord = wordbank[Math.floor(Math.random()*wordbank.length)];
 
+// create spaces for each character of current word
+document.getElementById("currentWord").innerHTML = "__ ".repeat(computerWord.length);
 
-
+// when the user presses a key, ensure key is lower case and search the computer word for the user's guess
 document.onkeyup = function (event) {
     var userGuess = event.key.toLowerCase();
     var foundinword = computerWord.search(userGuess);
-    
 }
 var inWord = false;
 
-for (var i=0, i < computerWord.length, i++) {
+for (var i=0; i < computerWord.length; i++) {
     if (userGuess===computerWord.charAt (i)) {
         inWord = true;
         document.getElementById("currentWord").innerHTML = userGuess
@@ -23,4 +27,5 @@ for (var i=0, i < computerWord.length, i++) {
     else {
 
     }
+}
 }
