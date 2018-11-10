c1a1394df05639de30133961a65a98d2df629229
jenmaz12 <jenmaz12@gmail.com>
2018-11-10 07:20:50 -0500

    adding comments

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,14 +1,16 @@
 // load window first
 window.onload = function() {
 // define word bank array
-var wordbank = ["chocolate","caramel","gumdrop","lollipop","licorice","taffy","toffee","skittles","smarties","twix","snickers","raisinets","fudge","butterfinger","dots","crunch","starburst"];
-// define user life points at start and minimum lifepoints (0)
+var wordbank = ["chocolate","caramel","gumdrop","lollipop","licorice","taffy","toffee","skittles","smarties","twix","snickers","raisinets","fudge","butterfinger","dots","crunch","starburst","payday"];
+// define user life points at start
 var lifepoints = 12;
+// winCount starts at 0
 var winCount = 0;
 // define array for wrong guesses to be pushed to
 var wrongGuess = [];
 // computer randomly selects word from word bank for current word
 var computerWord = wordbank[Math.floor(Math.random()*wordbank.length)];
+// declare userGuess variable
 var userGuess;
 // Boolean variable for whether or not the userGuess is in the computer word; starts as false
 var inWord = false;
@@ -24,14 +26,14 @@ document.getElementById("winCount").innerHTML= winCount;
 document.onkeyup = function (event) {
     userGuess = event.key.toLowerCase();
 
-// define foundinword as index number where userGuess is or is not (-1) in the computerword
-var foundinword = computerWord.search(userGuess);
-var wrongguesses = document.getElementById("wrong-guesses");
+    // define foundinword as index number where userGuess is or is not (-1) in the computerword
+    var foundinword = computerWord.search(userGuess);
+    var wrongguesses = document.getElementById("wrong-guesses");
 
-// if the index of the userGuess in computerWord is -1, it is not in the word
+    // if the index of the userGuess in computerWord is -1, it is not in the word
     if (foundinword===-1) {
         inWord = false;
-    //    userGuess is added to wrongGuess array
+        // userGuess is added to wrongGuess array
         wrongGuess.push(userGuess);
         // wrongGuess array is shown in wrongguesses p element
         wrongguesses.textContent = wrongGuess;
@@ -61,17 +63,7 @@ var wrongguesses = document.getElementById("wrong-guesses");
             }
             document.getElementById("currentWord").innerHTML = placeholderArray.join(" ");
             }
-        // if lifepoints are greater than zero, subtract 1 for each guess
-        if (lifepoints>0) {
-            lifepoints = lifepoints - 1;
-            document.getElementById("lifePoints").innerHTML = "Lifepoints: " + lifepoints;
-        }
-        else if (lifepoints<=0) {
-            alert("You lose!");
-            location.reload();
-        }
-        
-        // if there are no more __ in the placeholder array and the user still has lifepoints left, the user has guessed all of the letters in the word an has won
+        // if there are no more __ in the placeholder array and the user has 0 or positive lifepoints left, the user has guessed all of the letters in the word and has won
         if (placeholderArray.indexOf("__")===-1 && lifepoints>=0) {
             // alert user they won
             document.onload = alert("You win!!");
@@ -80,7 +72,19 @@ var wrongguesses = document.getElementById("wrong-guesses");
             // increase winCount by 1 after page reloads
             document.getElementById("winCount").innerHTML = winCount + 1;
             console.log(winCount);
-        }   
+        }  
+        // if lifepoints are greater than zero, subtract 1 for each guess
+        else if (lifepoints>0 && placeholderArray.indexOf("__")>-1) {
+            lifepoints = lifepoints - 1;
+            document.getElementById("lifePoints").innerHTML = "Lifepoints: " + lifepoints;
+            
+        }
+        else if (lifepoints<=0 && placeholderArray.indexOf("__")>-1) {
+            alert("You lose!");
+            location.reload();
+        }
+        
+         
 
     };
 };
