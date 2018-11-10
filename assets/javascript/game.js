fc57063f3db824555f5e5a25d7454c7c9deb74e8
jenmaz12 <jenmaz12@gmail.com>
2018-11-09 20:40:25 -0500

    filling in word

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -14,6 +14,9 @@ var inWord = false;
 // create spaces for each character of current word
 document.getElementById("currentWord").innerHTML = "__ ".repeat(computerWord.length);
 
+// show beginning life points
+document.getElementById("lifePoints").innerHTML= lifepoints;
+
 // when the user presses a key, ensure key is lower case
 document.onkeyup = function (event) {
     userGuess = event.key.toLowerCase();
@@ -23,34 +26,29 @@ var foundinword = computerWord.search(userGuess);
 var wrongguesses = document.getElementById("wrong-guesses");
 
 
-    // if (foundinword===-1) {
-    //     inWord = false;
-    //     wrongGuess.push(userGuess);
-    //     wrongguesses.textContent = wrongGuess;
-    // }
-    // else {
-    //     inWord = true;
-    //     var placeholderword = document.getElementById("currentWord").innerHTML;
-    //     var placeholderArray = placeholderword.split(" ");
-    //     placeholderArray[foundinword] = userGuess;
-    //     document.getElementById("currentWord").innerHTML = placeholderArray.join(" ");
-    // }
-// loop through length of computerword
-for (var i=0; i<computerWord.length; i++) {
-    var wordArray = computerWord.split("");
-    if (userGuess===wordArray.indexOf(i)) {
+    if (foundinword===-1) {
+        inWord = false;
+        wrongGuess.push(userGuess);
+        wrongguesses.textContent = wrongGuess;
+        
+    } else {
         inWord = true;
         var placeholderword = document.getElementById("currentWord").innerHTML;
         var placeholderArray = placeholderword.split(" ");
-        placeholderArray[i]=userGuess; 
-        document.getElementById("currentWord").innerHTML = placeholderArray.join(" ");
-       
-    }
-    else {
-        // inWord = false;
-        // wrongGuess.push(userGuess);
-        // wrongguesses.textContent = wrongGuess;  
-    }
-};
+        // loop through the length of the computerword
+        for (var i=0; i<computerWord.length; i++) {
+            // split computerword while in the loop
+            var wordArray = computerWord.split("");
+            debugger
+            // if userGuess equals the index of the split up computerword
+            if (userGuess===wordArray[i]) {
+                debugger
+                // then placeholder array equal userGuess
+                placeholderArray[i]=userGuess;
+            }
+            document.getElementById("currentWord").innerHTML = placeholderArray.join(" ");
+        }   
+
+    };
 };
 };
