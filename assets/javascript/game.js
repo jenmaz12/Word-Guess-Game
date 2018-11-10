2a673fb3f259b521c249840f22905bca0ebe407c
jenmaz12 <jenmaz12@gmail.com>
2018-11-10 06:53:59 -0500

    trying to figure out win count

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,7 +1,7 @@
 // load window first
 window.onload = function() {
 // define word bank array
-var wordbank = ["chocolate","caramel","gumdrop","lollipop","licorice","taffy","toffee","skittles","smarties","twix","snickers","raisinets"];
+var wordbank = ["chocolate","caramel","gumdrop","lollipop","licorice","taffy","toffee","skittles","smarties","twix","snickers","raisinets","fudge","butterfinger","dots","crunch","starburst"];
 // define user life points at start and minimum lifepoints (0)
 var lifepoints = 12;
 var winCount = 0;
@@ -28,15 +28,19 @@ document.onkeyup = function (event) {
 var foundinword = computerWord.search(userGuess);
 var wrongguesses = document.getElementById("wrong-guesses");
 
-
+// if the index of the userGuess in computerWord is -1, it is not in the word
     if (foundinword===-1) {
         inWord = false;
+    //    userGuess is added to wrongGuess array
         wrongGuess.push(userGuess);
+        // wrongGuess array is shown in wrongguesses p element
         wrongguesses.textContent = wrongGuess;
+        // if the user's lifepoints are greater than 0, one will be deducted for each guess and new total displayed
         if (lifepoints>0) {
             lifepoints = lifepoints - 1;
             document.getElementById("lifePoints").innerHTML = "Lifepoints: " + lifepoints;
         }
+        // if lifepoints are less than or equal to zero then the user receives a "You lose!" alert and page reloads
         else if (lifepoints<=0) {
             alert("You lose!");
             location.reload();
@@ -46,15 +50,6 @@ var wrongguesses = document.getElementById("wrong-guesses");
         inWord = true;
         var placeholderword = document.getElementById("currentWord").innerHTML;
         var placeholderArray = placeholderword.split(" ");
-        // if lifepoints are greater than zero, subtract 1 for each guess
-        if (lifepoints>0) {
-            lifepoints = lifepoints - 1;
-            document.getElementById("lifePoints").innerHTML = "Lifepoints: " + lifepoints;
-        }
-        else if (lifepoints<=0) {
-            alert("You lose!");
-            location.reload();
-        }
         // loop through the length of the computerword
         for (var i=0; i<computerWord.length; i++) {
             // split computerword while in the loop
@@ -65,11 +60,26 @@ var wrongguesses = document.getElementById("wrong-guesses");
                 placeholderArray[i]=userGuess;
             }
             document.getElementById("currentWord").innerHTML = placeholderArray.join(" ");
-            if (placeholderArray.indexOf("__")===-1 && lifepoints>=0) {
-                document.onload = alert("You win!!");
-                location.reload();
-                document.getElementById("wincount").innerHTML = winCount + 1;
             }
+        // if lifepoints are greater than zero, subtract 1 for each guess
+        if (lifepoints>0) {
+            lifepoints = lifepoints - 1;
+            document.getElementById("lifePoints").innerHTML = "Lifepoints: " + lifepoints;
+        }
+        else if (lifepoints<=0) {
+            alert("You lose!");
+            location.reload();
+        }
+        
+        // if there are no more __ in the placeholder array and the user still has lifepoints left, the user has guessed all of the letters in the word an has won
+        if (placeholderArray.indexOf("__")===-1 && lifepoints>=0) {
+            // alert user they won
+            document.onload = alert("You win!!");
+            // reload page to choose new word
+            location.reload();
+            // increase winCount by 1 after page reloads
+            document.getElementById("winCount").innerHTML = winCount + 1;
+            console.log(winCount);
         }   
 
     };
